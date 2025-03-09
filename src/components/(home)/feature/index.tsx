55ce1da35122bd53a1858dc2feb27c2c7e3da318
Thuy trinh ne <21522719@gm.uit.edu.vn>
2025-03-09 17:08:54 +0700

    feat(home) update fontsize

--- src/components/(home)/feature/index.tsx
+++ src/components/(home)/feature/index.tsx
@@ -45,7 +45,7 @@ export default function Features() {
                   strokeWidth={1.5}
                 />
               </div>
-              <h3 className="text-2xl font-bold mb-4 dark:text-gray-300 text-pri-1">
+              <h3 className="text-xl font-bold mb-4 dark:text-gray-300 text-pri-1">
                 {feature.title}
               </h3>
               <p className="text-black dark:text-gray-400">
