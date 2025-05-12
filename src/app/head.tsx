621f7594a15cd79fe6f9ec4433c13115fc4db02e
Thuy trinh ne <21522719@gm.uit.edu.vn>
2025-05-12 20:48:06 +0700

    add gg search console

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="i0ZneG0eia7xZg2Ly5GRuejfEBvKlBb0RJ7cPx1JtFA"
+      />
+    </>
+  );
+}
