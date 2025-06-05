a572cdfff4704e1ff2305c948ec05a743ec1bd16
Thuy trinh ne <21522719@gm.uit.edu.vn>
2025-06-05 11:40:56 +0700

    feat(gg) add Meta Pixel Code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
         src="https://www.googletagmanager.com/gtag/js?id=AW-17126219862"
         strategy="afterInteractive"
       />
-      <Script id="gtag-init" strategy="afterInteractive">
+      <Script id="gtag-init-2" strategy="afterInteractive">
         {`
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
@@ -79,6 +79,30 @@ export default function RootLayout({
           window.gtag_report_conversion = gtag_report_conversion; // expose function globally
         `}
       </Script>
+
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1900359257367063');
+    fbq('track', 'PageView');
+  `}
+      </Script>
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1900359257367063&ev=PageView&noscript=1"
+        />
+      </noscript>
 
       <body className="ml:bg-white md:bg-bg-1 dark:bg-bg-dark-1">
         <PageLoader>
