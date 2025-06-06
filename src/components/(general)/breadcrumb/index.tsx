90da154092c26cf126bc68eb636fe8074b8c6b66
gembytram <tramcutee0512@gmail.com>
2025-06-06 14:16:16 +0700

    Breadcrumb

--- src/components/(general)/breadcrumb/index.tsx
+++ src/components/(general)/breadcrumb/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import {
   Breadcrumb,
   BreadcrumbEllipsis,
@@ -22,16 +23,12 @@ export default function BreadCrumb({
   breadcrumbs: { label: string; href?: string }[];
 }) {
   const MAX_VISIBLE_BREADCRUMBS = 3;
-
   const shouldTruncate = breadcrumbs.length > MAX_VISIBLE_BREADCRUMBS;
 
-  // Tách breadcrumb thành phần đầu, liền kề cuối và cuối cùng
   const firstBreadcrumb = breadcrumbs[0];
   const secondToLastBreadcrumb =
     shouldTruncate && breadcrumbs[breadcrumbs.length - 2];
   const lastBreadcrumb = breadcrumbs[breadcrumbs.length - 1];
-
-  // Breadcrumbs cần hiển thị trong menu toggle (nếu có)
   const hiddenBreadcrumbs =
     shouldTruncate && breadcrumbs.slice(1, breadcrumbs.length - 2);
 
@@ -39,16 +36,17 @@ export default function BreadCrumb({
     <div className="flex mt-24 w-4/5 mx-auto">
       <Breadcrumb className="ml-4">
         <BreadcrumbList>
-          {/* Hiển thị breadcrumb đầu tiên */}
+          {/* Breadcrumb đầu tiên */}
           <BreadcrumbItem>
             <BreadcrumbLink
               href={firstBreadcrumb.href}
-              className="text-gray-500 hover:text-teal-600 dark:text-gray-200 dark:hover:text-teal-200">
+              className="text-gray-500 hover:text-teal-600 dark:text-gray-200 dark:hover:text-teal-200"
+            >
               {firstBreadcrumb.label}
             </BreadcrumbLink>
           </BreadcrumbItem>
 
-          {/* Dấu "..." nếu cần ẩn */}
+          {/* Dấu ... nếu cần ẩn */}
           {shouldTruncate && hiddenBreadcrumbs && (
             <>
               <BreadcrumbSeparator />
@@ -61,11 +59,12 @@ export default function BreadCrumb({
                   <DropdownMenuContent align="start">
                     {hiddenBreadcrumbs.map((crumb, index) => (
                       <DropdownMenuItem key={index}>
-                        <BreadcrumbLink
-                          href={crumb.href}
-                          className="text-gray-500 hover:text-teal-600 dark:text-gray-300">
+                        <Link
+                          href={crumb.href || "#"}
+                          className="text-gray-500 hover:text-teal-600 dark:text-gray-300"
+                        >
                           {crumb.label}
-                        </BreadcrumbLink>
+                        </Link>
                       </DropdownMenuItem>
                     ))}
                   </DropdownMenuContent>
@@ -74,21 +73,22 @@ export default function BreadCrumb({
             </>
           )}
 
-          {/* Hiển thị breadcrumb liền kề cuối (nếu có) */}
+          {/* Breadcrumb gần cuối */}
           {shouldTruncate && secondToLastBreadcrumb && (
             <>
               <BreadcrumbSeparator />
               <BreadcrumbItem>
                 <BreadcrumbLink
                   href={secondToLastBreadcrumb.href}
-                  className="text-gray-500 hover:text-teal-600 dark:text-gray-300">
+                  className="text-gray-500 hover:text-teal-600 dark:text-gray-300"
+                >
                   {secondToLastBreadcrumb.label}
                 </BreadcrumbLink>
               </BreadcrumbItem>
             </>
           )}
 
-          {/* Hiển thị breadcrumb cuối cùng */}
+          {/* Breadcrumb cuối */}
           <BreadcrumbSeparator />
           <BreadcrumbItem>
             <BreadcrumbPage className="text-black font-semibold dark:text-gray-400">
