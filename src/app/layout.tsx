143fcc8f121b1b765201e7614bce3f288109615a
Thuy trinh ne <21522719@gm.uit.edu.vn>
2025-06-08 10:34:47 +0700

    feat(fb pixel) update fb pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
     t.src=v;s=b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t,s)}(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1900359257367063');
+    fbq('init', '2079727345770874');
     fbq('track', 'PageView');
   `}
       </Script>
@@ -100,7 +100,7 @@ export default function RootLayout({
           height="1"
           width="1"
           style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=1900359257367063&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=2079727345770874&ev=PageView&noscript=1"
         />
       </noscript>
 
